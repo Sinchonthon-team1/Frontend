6c49bba577447d9aca9ef81b077b271fe4fcb8f5
letthem <suyeon3662@naver.com>
2024-08-24 03:00:23 +0900

    Feat: AllUnivCard 내부 UI 구현

--- src/pages/Main/AllUnivCard.jsx
+++ src/pages/Main/AllUnivCard.jsx
@@ -1,9 +1,23 @@
 import styled from "styled-components";
-
+import MainButton from "../../components/MainButton";
+import univs from "../../assets/images//Main/univs.svg";
 const AllUnivCard = () => {
   return (
     <AllUnivCardWrapper>
-      <AllUnivSquare></AllUnivSquare>
+      <AllUnivSquare>
+        <Title>
+          참여 대학을
+          <br />
+          확인해 보세요
+        </Title>
+        <MoreBtn>더보기</MoreBtn>
+        <MakeTeamBtn>
+          <MainButton>팀 생성하기</MainButton>
+        </MakeTeamBtn>
+        <Puzzle>
+          <img src={univs} alt="univs" />
+        </Puzzle>
+      </AllUnivSquare>
       <AllUnivCircle />
     </AllUnivCardWrapper>
   );
@@ -25,8 +39,7 @@ const AllUnivSquare = styled.div`
   background-color: #ffe7bf;
   border-radius: 20px;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
   ${(props) => props.theme.fontStyles.subHead1};
 `;
 
@@ -39,4 +52,43 @@ const AllUnivCircle = styled.div`
   border-radius: 50%;
   background-color: #ffe7bf;
   cursor: pointer;
+`;
+
+const Title = styled.p`
+  ${(props) => props.theme.fontStyles.headline3};
+  margin-top: 32px;
+  margin-left: 20px;
+`;
+
+const MakeTeamBtn = styled.div`
+  position: absolute;
+  bottom: 20px;
+  left: 16px;
+`;
+
+const MoreBtn = styled.div`
+  margin-top: 33px;
+  margin-left: 256px;
+  width: 80px;
+  height: 31px;
+  ${(props) => props.theme.fontStyles.body2Bold};
+  color: ${(props) => props.theme.colors.main};
+  box-shadow: 0 0 0 1px ${(props) => props.theme.colors.main} inset;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  border-radius: 28px;
+  &:hover {
+    background: rgba(255, 16, 140, 0.25);
+  }
+`;
+
+const Puzzle = styled.div`
+  margin-top: 4px;
+  margin-left: 27px;
+  width: 330px;
+  height: 315px;
+  position: relative;
+  z-index: 1;
 `;
